03cfc97d3c9a1e05814dd505584fd96043432270
nealgandhi <gandhi.neal2002@gmail.com>
2023-01-25 19:01:39 -0600

    Removed Custom Colors causing styling errors

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-azure absolute p-1 color-white inset-x-0 bottom-0">
+    <div className="bg-blue-500 text-white absolute p-1 inset-x-0 bottom-0">
       {" "}
       Created by{" "}
       <a href="https://www.linkedin.com/in/neal-gandhi">Neal Gandhi</a> for the{" "}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
-    colors: {
-      argent: "#7b7979",
-      azure: "#0681c6",
-    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
